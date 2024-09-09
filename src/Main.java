97f67e93fac9024807ea4942760e9ab9f7158d79
AraFrankow2 <arabelatatiana.frank@davinci.edu.ar>
2024-09-09 10:11:11 -0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hola");
 	}
 
 }
